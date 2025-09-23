17f35855473cd84a4c2c95a359066f94056875d4
and_wu <and36uwu@gmail.com>
2025-09-23 17:23:59 +0300

    добавил настройку логирования

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import asyncio
+import logging
 
 from aiogram import Dispatcher, Bot
 from aiogram.client.default import DefaultBotProperties
@@ -6,12 +7,18 @@ from aiogram.client.default import DefaultBotProperties
 from config_data.config import BOT_TOKEN
 from src.handlers.commands import router as commands_router
 
+
+# Настройка логирования
+logging.basicConfig(level=logging.INFO)
+
+
 dp = Dispatcher()
 bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode='HTML'))
 
 
 async def start():
     dp.include_router(commands_router)
+
 
     await bot.delete_webhook(drop_pending_updates=True)
     print(await bot.me())
