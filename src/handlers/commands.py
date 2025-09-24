18cdf0b8b3a9d7fee59aa88b7be56ac354f2f465
and_wu <and36uwu@gmail.com>
2025-09-24 11:39:30 +0300

    добавил пока пустой хэндлер для обработки фотографий

--- src/handlers/commands.py
+++ src/handlers/commands.py
@@ -1,4 +1,4 @@
-from aiogram import Router
+from aiogram import Router, F
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 
@@ -15,4 +15,30 @@ async def send_welcome(message: Message):
 async def send_help(message: Message):
     await message.answer(f"я бот который считывает инфу с чета, просто пришли мне его фото")
 
+
+@router.message(F.photo)
+async def process_photo(message: Message):
+    await message.answer(f'Обрабатываю полученное фото')
+
+    # берём фото в наилучшем качестве
+    photo = message.photo[-1]
+    file = await message.bot.get_file(photo.file_id)
+
+    # скачиваем фото
+    file_path = file.file_path
+    downloaded = await message.bot.download_file(file_path)
+
+    # сохраняем временно
+    image_path = "check.jpg"
+    with open(image_path, "wb") as f:
+        f.write(downloaded.read())
+
+    # OCR (распознавание текста)
+    text = pytesseract.image_to_string(Image.open(image_path), lang="rus+eng")
+
+    # Отправляем результат
+    if text.strip():
+        await message.answer(f"📄 Я нашёл такой текст:\n\n<code>{text}</code>")
+    else:
+        await message.answer("⚠️ Не удалось распознать текст на чеке.")
 
